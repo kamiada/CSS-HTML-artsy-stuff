68ba22187ae54396fa5cebcb4c4022ed9f7a5d5d
Kamiada <kamiada528@gmail.com>
2020-08-30 15:45:56 +0100

    trying to fix shadow

--- BouncingBall/main.js
+++ BouncingBall/main.js
@@ -3,4 +3,6 @@ function playAnimation(){
     audio.play();
     var ball = document.getElementById("ball_figure");
     ball.classList.add("ball-active");
+    var shadow = document.getElementById("shadow");
+    shadow.classList.add("shadow-active");
 }
